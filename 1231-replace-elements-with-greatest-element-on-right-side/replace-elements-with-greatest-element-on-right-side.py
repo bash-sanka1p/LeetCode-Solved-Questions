611e1f9cc976f8aff52ace24a536e0d13e4b574e
Sankalp Singh Bais <70506573+bash-sanka1p@users.noreply.github.com>
2024-05-27 20:19:07 +0530

    Time: 502 ms (73.14%) | Memory: 18 MB (88.40%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -1,11 +1,16 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        maxele=max(arr)
-        for i in range(len(arr)):
-            if i==len(arr)-1:
-                arr[i]=-1
-                return arr
-            if maxele==arr[i]:
-                maxele=max(arr[i+1:])
-            arr[i] = maxele
-        
+        # maxele=max(arr)
+        # for i in range(len(arr)):
+        #     if i==len(arr)-1:
+        #         arr[i]=-1
+        #         return arr
+        #     if maxele==arr[i]:
+        #         maxele=max(arr[i+1:])
+        #     arr[i] = maxele
+        maxele=-1
+        for i in range(len(arr)-1, -1, -1):
+            tempmax=max(arr[i],maxele)
+            arr[i]=maxele
+            maxele=tempmax
+        return arr
