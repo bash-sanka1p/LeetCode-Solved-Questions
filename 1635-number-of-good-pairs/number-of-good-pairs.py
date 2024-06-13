86e2ceb2694f23ef15504ed3d83858b81c9a97d0
Sankalp Singh Bais <70506573+bash-sanka1p@users.noreply.github.com>
2024-06-13 09:32:33 +0530

    Time: 31 ms (88.31%) | Memory: 16.5 MB (76.76%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -4,7 +4,7 @@ class Solution:
         pairs=0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if nums[i]==nums[j] and i<j:
+                if nums[i]==nums[j]:
                     pairs+=1
     
         return pairs
