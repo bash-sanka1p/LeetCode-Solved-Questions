b9c508fb8f039693d7cb56d2bfd4c3878f0ab462
Sankalp Singh Bais <70506573+bash-sanka1p@users.noreply.github.com>
2025-02-08 22:41:58 +0530

    Time: 34 ms (5.39%) | Memory: 18.3 MB (6.00%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -3,8 +3,8 @@ class Solution:
        count = 0
        for i in range(len(s)):
         if i%2==0:
-            count += 1 if s[i]!='1' else 0
+            count += 1 if s[i]=='0' else 0
         else:
-            count += 1 if s[i]!='0' else 0
+            count += 1 if s[i]=='1' else 0
         print(count)
        return min(count, len(s)-count)
