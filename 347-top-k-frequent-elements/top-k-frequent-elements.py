ce049161070943677b6c09dc13ecd9214849cc4e
Sankalp Singh Bais <70506573+bash-sanka1p@users.noreply.github.com>
2025-02-17 23:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (80.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,7 +3,6 @@ class Solution:
         from  collections import Counter
         freq = Counter(nums)
         sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-        print(sorted_freq)
         res = [x for x, _ in sorted_freq[:k]]
         return res
         
